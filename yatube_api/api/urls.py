c0571fbd2e5f09deae008dc9c7530ce2c9f8ccd6
Barinov Maksim <craftyplonkton@gmail.com>
2022-04-29 02:08:59 +0300

    add exempls

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -5,8 +5,8 @@ from rest_framework import routers
 from api.views import GroupViewSet, PostViewSet, CommentViewSet, FollowView
 
 router_v1 = routers.DefaultRouter()
-router_v1.register(r'groups', GroupViewSet)
-router_v1.register(r'posts', PostViewSet)
+router_v1.register(r'groups', GroupViewSet, basename='group')
+router_v1.register(r'posts', PostViewSet, basename='post')
 router_v1.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
